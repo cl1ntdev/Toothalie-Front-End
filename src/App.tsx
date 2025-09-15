40264e80f6e5c4712d7bf6d4cb0a5699dff676ae
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-15 08:40:42 +0800

    inital vite+tailwind

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-gray-900'>
+        <p className='text-2xl'>HELLO WORLD</p>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
