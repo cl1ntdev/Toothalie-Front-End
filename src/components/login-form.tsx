cc31a900998d26829ffe752cdcd2202b6ce93c47
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-16 22:16:29 +0800

    fix

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -3,6 +3,9 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Link } from "react-router-dom"
+
+
+
 export function LoginForm({
   className,
   ...props
