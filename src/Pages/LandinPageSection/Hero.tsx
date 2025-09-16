db162bad8cbe1104365f47fb7ca24c506bb06e2a
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-16 19:55:57 +0800

    !responsive comment

--- src/Pages/LandinPageSection/Hero.tsx
+++ src/Pages/LandinPageSection/Hero.tsx
@@ -22,6 +22,12 @@ export default function HeroSection() {
           <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md text-sm transition-colors hidden sm:block">
             Sign up
           </button>
+          
+          {/* <><><><><><><><><><><><><><><><><><><><><><><><><><><> */}
+          {/*  THIS CONTENT HERE IS STILL UNCLICKABLE IN MOBILE VIEW */}
+          {/* <><><><><><><><><><><><><><><><><><><><><><><><><><><> */}
+          
+          
           <button className="md:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
