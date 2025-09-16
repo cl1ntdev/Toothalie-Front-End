7dd944399946b3c88ccc3dec317ded18e5b05014
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-16 18:55:16 +0800

    first section of the page

---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from './LandinPageSection/Hero'
+
+export default function LandingPage(){
+  return(
+    <>
+      <div>
+        <HeroSection />             
+      </div>
+    </>
+  )
+}
