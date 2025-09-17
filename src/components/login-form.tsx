a13f31bb1e595afd6f9cd08fa1e6bf296f87c59b
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-17 18:50:02 +0800

    fix login-from

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -4,38 +4,42 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Link } from "react-router-dom"
 
-
+import checkUserLogin from "@/API/LoginAuth" // checking username and password only
+import { UserLoginInfo } from "@/Classes/UserLogin"
+import { useState } from "react"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
+  const [userName,setUsername] = useState<string>("")
+  const [password,setPassword] = useState<string>("")
+  
+  const handleLogin = async() =>{
+    const user = new UserLoginInfo(userName,password)
+    await checkUserLogin(user)
+  }
+  
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">Login your account</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          Enter your email below to Register to your account
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input onChange={(e)=>setUsername(e.target.value)} id="email" type="email" placeholder="m@example.com" required />
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
           </div>
-          <Input id="password" type="password" required />
+          <Input onChange={(e)=>setPassword(e.target.value)} id="password" type="password" required />
         </div>
-        <Button type="submit" className="w-full">
+        <Button onClick={()=>handleLogin()} type="submit" className="w-full">
           Login
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
