64290cdea443626f5fad5ee6109d721a4e5a041a
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-18 05:33:41 +0800

    initial request post

--- src/API/LoginAuth.ts
+++ src/API/LoginAuth.ts
@@ -1,10 +1,14 @@
-import { UserLoginInfo } from "@/Classes/UserLogin"
-export default async function checkUserLogin (value:UserLoginInfo){
+import { UserLoginInfo } from "@/Classes/UserLogin";
+export default async function checkUserLogin(value: UserLoginInfo) {
   const userInfo = value;
-    
-  await fetch("http://127.0.0.1:8000/api/login-auth",{
-    method:"POST",
-    headers: { "ContentType":"application/json"},
-    body:JSON.stringify(userInfo)
+
+  const response = await fetch("http://127.0.0.1:8000/api/login-auth", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userInfo),
   })
-} 
+  const result = await response.json()
+  console.log(result)
+  
+  return response;
+}
