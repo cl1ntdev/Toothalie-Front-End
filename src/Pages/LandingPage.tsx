b6e988399d0fb0151b247284ebb1f15360400120
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-19 11:34:57 +0800

    added second section

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import HeroSection from './LandinPageSection/Hero'
-
+import LPage_Section1 from "./LandinPageSection/Lpage_Section1";
 export default function LandingPage(){
   return(
     <>
       <div>
-        <HeroSection />             
+        <HeroSection /> 
+        <LPage_Section1 />
       </div>
     </>
   )
