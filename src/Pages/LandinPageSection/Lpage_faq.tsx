2f2d918411ea8018b8d00d213b84ba793215692b
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-19 12:11:32 +0800

    FAQ integration

---
+++ src/Pages/LandinPageSection/Lpage_faq.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Faq3 } from "@/components/faq3";
+
+export default function Lpage_faq() {
+  return (
+    <Faq3
+      heading="Frequently asked questions"
+      description="Find answers to common questions about our products. Can't find what you're looking for? Contact our support team."
+      supportHeading="Need more help?"
+      supportDescription="Our support team is available 24/7 to assist you."
+      supportButtonText="Contact support"
+      supportButtonUrl="/contact"
+    />
+  );
+}
--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from './LandinPageSection/Hero'
 import LPage_Section1 from "./LandinPageSection/Lpage_Section1";
 import LPage_Section2 from "./LandinPageSection/Lpage_Section2";
+import Lpage_faq from "./LandinPageSection/Lpage_faq";
 export default function LandingPage(){
   return(
     <>
@@ -9,6 +10,7 @@ export default function LandingPage(){
         <HeroSection /> 
         <LPage_Section1 />
         <LPage_Section2 />
+        <Lpage_faq />
       </div>
     </>
   )
