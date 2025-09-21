f6e9f4a95fa80ba52b7b289dcf3e204316da4278
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-22 05:36:58 +0800

    added home route to logo

--- src/Pages/Auth/LoginPage.tsx
+++ src/Pages/Auth/LoginPage.tsx
@@ -1,17 +1,17 @@
-import { GalleryVerticalEnd } from "lucide-react"
 import { LoginForm } from "@/components/login-form"
-
+import { Link } from "react-router-dom"
+import logo from '../../assets/logo2.png'
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
+          <Link to="/" className="flex items-center gap-2 font-medium">
+            <div className="text-primary-foreground flex size-15 p-2 items-center justify-center rounded-md">
+              <img src={logo}/>
             </div>
-            Acme Inc.
-          </a>
+            Toothalie
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
