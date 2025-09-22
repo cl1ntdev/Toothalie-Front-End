219203e7a74aa47f684758f05b89860f6ebed9a8
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-22 16:36:06 +0800

    register checker

---
+++ src/API/RegisterAuth.ts
@@ -0,0 +1,14 @@
+// import { UserLoginInfo } from "@/Classes/UserLogin";
+export default async function LoginAuth(value:string) {
+  const userInfo = value;
+
+  const response = await fetch("http://127.0.0.1:8000/api/reg-check-user", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userInfo),
+  })
+  const result = await response.json()
+  console.log(result)
+  
+  return response;
+}
