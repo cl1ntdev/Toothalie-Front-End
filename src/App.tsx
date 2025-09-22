7f82ce81beb2027b736f4d05e7df55f9414ddb3e
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-22 17:11:54 +0800

    fix missing element

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LandingPage from './Pages/LandingPage'
 import { Routes, Route } from "react-router-dom";
 import LoginPage from './Pages/Auth/LoginPage';
 import  RegisterPage  from './Pages/Auth/RegisterPage';
-
+import UserPage from './Pages/UserPage';
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        <Route path='/user/:id' /> {/* use LINK to navigate and then useParams to get the value */}
+        <Route path='/user/:id' element={<UserPage />} /> {/* use LINK to navigate and then useParams to get the value */}
       </Routes>
     </>
   )
