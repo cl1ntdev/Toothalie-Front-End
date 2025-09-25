b220e65ca9b8e7bf9cfedf21c87a2d0a32b71eb8
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-25 19:23:49 +0800

    userRole class

---
+++ src/Classes/UserRole.ts
@@ -0,0 +1,14 @@
+const AdminPermission = ["Create","Read","Update","Delete"]
+const OwnerPermission = ["Create","Read","Update","Delete"]
+
+const Roles = [
+  {
+    RoleName:"Admin",
+    Permissions: AdminPermission
+  },
+  {
+    RoleName:"Patient",
+    Permissions: OwnerPermission
+  },
+]
+
