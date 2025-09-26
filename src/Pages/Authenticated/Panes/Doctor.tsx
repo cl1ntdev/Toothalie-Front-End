14b12481cf13e4a32a649b3da76836b421075b39
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-27 00:46:24 +0800

    shadcn sidebar

--- src/Pages/Authenticated/Panes/Doctor.tsx
+++ src/Pages/Authenticated/Panes/Doctor.tsx
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { LoginedUserClass } from '@/Classes/Authenticated/LoginedUserInfoClass'
+import { AppSidebar } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+import data from "../../../app/dashboard/data.json"
+
 type DoctorPanelProps = {
   userLoginedInfo?: LoginedUserClass
 }
@@ -9,10 +21,29 @@ export default function DoctorPanel({ userLoginedInfo }:DoctorPanelProps){
   const user = userLoginedInfo
   
   return(
-    <>
-      <div>
-        <h1>Hello Doctor { user?.id}</h1>
-      </div>
-    </>
+    <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" />
+          <SidebarInset>
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  <SectionCards />
+                  <div className="px-4 lg:px-6">
+                    <ChartAreaInteractive />
+                  </div>
+                  <DataTable data={data} />
+                </div>
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
   )
 }
