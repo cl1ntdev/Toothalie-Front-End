50b9a8f50b362093bd59c338eb82b6acd0444dba
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-27 00:43:52 +0800

    theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from 'next-themes'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />      
-    </BrowserRouter>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <BrowserRouter>
+        <App />      
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
