91091431198ff6a21eca7ed75db1ce23e5979b1e
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-26 23:42:07 +0800

    separeted folder for clarity

---
+++ src/Classes/Authenticated/LoginedUserInfoClass.tsx
@@ -0,0 +1,13 @@
+class UserLoginInfoClass {
+  firstname: string
+  lastname: string
+  role: string
+  
+  constructor(firstname:string,lastname:string,role:string){
+    this.firstname= firstname
+    this.lastname= lastname
+    this.role= role
+  }
+}
+
+export {UserLoginInfoClass}
