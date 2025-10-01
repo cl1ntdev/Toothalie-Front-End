c0a228f1bf5c4d7970cb21aee4bdf848d1514c7d
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-01 14:18:37 +0800

    class for dentist

---
+++ src/Classes/Authenticated/DentistClass.tsx
@@ -0,0 +1,14 @@
+class Dentist {
+  name:string
+  email:string
+  experience:string
+  specialty:string
+  constructor(name:string,email:string,experience:string,specialty:string){
+    this.name = name
+    this.email = email
+    this.experience = experience
+    this.specialty = specialty
+  }
+}
+
+export default {Dentist}
