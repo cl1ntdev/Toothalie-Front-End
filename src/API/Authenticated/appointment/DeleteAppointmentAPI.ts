da3a0ac93569fc5a035fa1328d40eba11d7c0ba0
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-02 13:53:16 +0800

    post, sending appointment to delete

---
+++ src/API/Authenticated/appointment/DeleteAppointmentAPI.ts
@@ -0,0 +1,17 @@
+export default async function DeleteAppointmentAPI(appointmentID:string){
+  
+  console.log('Appointment id is: '+ appointmentID)
+  if(appointmentID){
+   const deleteAppointmentDetails = await fetch('http://127.0.0.1:8000/api/delete-appointment',{
+     method:"POST",
+     headers:{"Content-Type":"application/json"},
+     body: JSON.stringify({ appointmentID })
+   })
+     const data = await deleteAppointmentDetails.json() // only once
+     console.log("Appointment data:", data)
+ 
+     return data
+  }
+  
+  
+}
