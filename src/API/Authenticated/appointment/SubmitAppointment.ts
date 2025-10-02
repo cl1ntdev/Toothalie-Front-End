eb9f7c3b813428c3c9d144bcf7eb29eb6d6e32b2
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-02 14:40:40 +0800

    added time and day

--- src/API/Authenticated/appointment/SubmitAppointment.ts
+++ src/API/Authenticated/appointment/SubmitAppointment.ts
@@ -1,11 +1,12 @@
-export default async function SubmitAppointment(patientID:string, dentistID:string,schedule:string){
+export default async function SubmitAppointment(patientID:string, dentistID:string,day:string,time:string){
   console.log(patientID)
   console.log(dentistID)
-  console.log(schedule)
+  console.log(day)
+  console.log(time)
   const submit = await fetch('http://127.0.0.1:8000/api/add-appointment',{
     method:"POST",
     headers:{"Content-Type":"application/json"},
-    body: JSON.stringify({ patientID, dentistID, schedule})
+    body: JSON.stringify({ patientID, dentistID, day,time})
   })
   
   console.log(submit)
