c1d00d0d95daddd0b7012552dbab4521f8a1b9ea
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-02 09:00:45 +0800

    fetch appointment based on userid

---
+++ src/API/Authenticated/appointment/FetchAppointment.ts
@@ -0,0 +1,17 @@
+export default async function FetchAppointment(){
+  const userID = localStorage.getItem("userID")
+  console.log('userID is: '+ userID)
+  if(userID){
+   const fetchAppointmentDetails = await fetch('http://127.0.0.1:8000/api/get-appointment',{
+     method:"POST",
+     headers:{"Content-Type":"application/json"},
+     body:JSON.stringify({userID})
+   })
+   const data = await fetchAppointmentDetails.json() // only once
+     console.log("Appointment data:", data)
+ 
+     return data
+  }
+  
+  
+}
