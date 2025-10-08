d9f195631e3414eba2065a6b3e23afb78a6175bb
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-09 06:16:56 +0800

    initial

--- src/API/RegisterAuth.ts
+++ src/API/RegisterAuth.ts
@@ -1,7 +1,7 @@
 import Patient from "@/Classes/Authenticated/Patient";
-export default async function LoginAuth(value:Patient) {
+export default async function RegisterAuth(value:Patient) {
   const userInfo = value;
-
+  console.log(userInfo)
   const response = await fetch("http://127.0.0.1:8000/api/reg-check-user", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,11 +1,14 @@
+// NOT FINISHED, LATER FOR REGISTER AND AUTH
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Link, useNavigate} from "react-router-dom"
 import { useState } from "react"
-import LoginAuth from "@/API/LoginAuth"
 import Patient from "@/Classes/Authenticated/Patient"
+import RegisterAuth from "@/API/RegisterAuth"
+
 export function RegisterForm({
   className,
   ...props
@@ -24,7 +27,7 @@ export function RegisterForm({
     console.log('working')
     if(password.trim() == confPassword.trim()){
       const user = new Patient(userName,firstName,lastName,password,contactNo,email)
-      const UserLoginData = await LoginAuth(user)
+      const UserLoginData = await RegisterAuth(user)
       console.log("userlogininfo," + UserLoginData)
       if(UserLoginData){
         alert("account exists")
@@ -52,6 +55,7 @@ export function RegisterForm({
       <div className="grid gap-6">
     
         {/* Username */}
+       
         <div className="grid gap-3">
           <Label htmlFor="username">Username</Label>
           <Input
@@ -63,55 +67,59 @@ export function RegisterForm({
           />
         </div>
     
-        {/* First Name */}
-        <div className="grid gap-3">
-          <Label htmlFor="first_name">First Name</Label>
-          <Input
-            id="first_name"
-            type="text"
-            placeholder="Enter your first name"
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
+        <div className="grid-cols-2 flex gap-2 ">
+          <div className="">
+            <Label htmlFor="first_name" className="mb-2">First Name</Label>
+            <Input
+              id="first_name"
+              type="text"
+              placeholder="Enter your first name"
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
+      
+          <div className="grid">
+            <Label htmlFor="last_name" className="mb-2">Last Name</Label>
+            <Input
+              id="last_name"
+              type="text"
+              placeholder="Enter your last name"
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
         </div>
+        
     
-        {/* Last Name */}
-        <div className="grid gap-3">
-          <Label htmlFor="last_name">Last Name</Label>
-          <Input
-            id="last_name"
-            type="text"
-            placeholder="Enter your last name"
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
+    
+        <div className="grid-cols-2 flex">
+          {/* Contact Number */}
+          <div className="px-2">
+            <Label htmlFor="contact_no" className="mb-2">Contact Number</Label>
+            <Input
+              id="contact_no"
+              type="text"
+              placeholder="e.g. 09XXXXXXXXX"
+              onChange={(e) => setContactNo(e.target.value)}
+              required
+            />
+          </div>
+      
+          {/* Email */}
+          <div className="px-2">
+            <Label htmlFor="email" className="mb-2">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+      
         </div>
-    
-    
-        {/* Contact Number */}
-        <div className="grid gap-3">
-          <Label htmlFor="contact_no">Contact Number</Label>
-          <Input
-            id="contact_no"
-            type="text"
-            placeholder="e.g. 09XXXXXXXXX"
-            onChange={(e) => setContactNo(e.target.value)}
-            required
-          />
-        </div>
-    
-        {/* Email */}
-        <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="m@example.com"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-    
+        
         {/* Password */}
         <div className="grid gap-3">
           <Label htmlFor="password">Password</Label>
@@ -139,21 +147,7 @@ export function RegisterForm({
           Register
         </Button>
     
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Or continue with
-          </span>
-        </div>
-    
-        <Button variant="outline" className="w-full text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-              fill="currentColor"
-            />
-          </svg>
-          Login with GitHub
-        </Button>
+       
       </div>
     
       <div className="text-center text-sm">
