dca6599bd96db41c529c0568f5ca4e57f7c422cc
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-09 10:59:30 +0800

    sending new user input

--- src/API/Authenticated/appointment/SubmitAppointment.ts
+++ src/API/Authenticated/appointment/SubmitAppointment.ts
@@ -1,12 +1,32 @@
-export default async function SubmitAppointment(patientID:string, dentistID:string,day:string,time:string){
+export default async function SubmitAppointment(
+  patientID:string, 
+  dentistID:string,
+  day:string,
+  time:string,
+  emergency:boolean,
+  familyBooking:boolean,
+  date:string | null
+
+){
   console.log(patientID)
   console.log(dentistID)
   console.log(day)
   console.log(time)
+  console.log(emergency)
+  console.log(familyBooking)
+  console.log(date)
   const submit = await fetch('http://127.0.0.1:8000/api/add-appointment',{
     method:"POST",
     headers:{"Content-Type":"application/json"},
-    body: JSON.stringify({ patientID, dentistID, day,time})
+    body: JSON.stringify({
+      patientID, 
+      dentistID,
+      day,
+      time,
+      emergency,
+      familyBooking,
+      date
+    })
   })
   
   console.log(submit)
