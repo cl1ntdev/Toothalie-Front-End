ca47b3d79c4b71413153428583064ac3e4db5189
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-12 18:49:02 +0800

    directly move to specific panel

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from './Pages/Auth/LoginPage';
 import  RegisterPage  from './Pages/Auth/RegisterPage';
 import UserPage from './Pages/Authenticated/UserPage';
-
+import PatientPanel from './Pages/Authenticated/Panes/PatientPane/Patient';
 function App() {
 
   return (
@@ -14,7 +14,8 @@ function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        <Route path='/user/:id' element={<UserPage />} /> {/* use LINK to navigate and then useParams to get the value */}
+        {/* <Route path='/user/:id' element={<UserPage />} /> {/* use LINK to navigate and then useParams to get the value */} */}
+        <Route path='/patient/:id' element={<PatientPanel />} />
       </Routes>
     </>
   )
