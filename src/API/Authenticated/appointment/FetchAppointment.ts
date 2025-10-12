a39955757824a2fd4658e8c3a4a15a8976898f4a
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-12 21:08:06 +0800

    fetch data for dentist pane

--- src/API/Authenticated/appointment/FetchAppointment.ts
+++ src/API/Authenticated/appointment/FetchAppointment.ts
@@ -1,4 +1,4 @@
-export default async function FetchAppointment(){
+async function FetchAppointment(){
   const userID = localStorage.getItem("userID")
   console.log('userID is: '+ userID)
   if(userID){
@@ -13,5 +13,19 @@ export default async function FetchAppointment(){
      return data
   }
   
-  
 }
+
+async function fetchAppointmentDentist(dentistID:string){
+  const fetchAppointmentDetails = await fetch('http://127.0.0.1:8000/api/get-appointment-dentist',{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({dentistID})
+  })
+  const data = await fetchAppointmentDetails.json() // only once
+    console.log("Appointment data:", data)
+
+    return data
+ 
+}
+
+export { FetchAppointment, fetchAppointmentDentist };
