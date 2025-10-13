9a006bf4d9d6453db4b6cc38c8c9d9a4914fd3fd
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-13 18:36:43 +0800

    initial for reminder tab

---
+++ src/Pages/Authenticated/Panes/PatientPane/Reminder.tsx
@@ -0,0 +1,11 @@
+import React, { useState,useEffect } from "react";
+
+
+export default function ReminderPane() {
+ 
+  return (
+    <>
+      <h1>FEEDS FOR REMINDER BY DENTISTS</h1>
+    </>
+  );
+}
