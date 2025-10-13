4468ecea777b6c8c749d5a58d58284ac999a3ab0
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-13 18:29:10 +0800

    inital page for History

---
+++ src/Pages/Authenticated/Panes/PatientPane/History.tsx
@@ -0,0 +1,11 @@
+import React, { useState,useEffect } from "react";
+
+
+export default function HistoryPane() {
+ 
+  return (
+    <>
+      <h1>HISTORY OF APPROVED APPOINTMENTS</h1>
+    </>
+  );
+}
