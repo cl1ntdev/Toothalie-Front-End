2568ab28a3f714a3d050ee442b43991883e28c29
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-13 17:21:15 +0800

    dentist update appointment status

--- src/API/Authenticated/appointment/EditAppointmentAPI.ts
+++ src/API/Authenticated/appointment/EditAppointmentAPI.ts
@@ -1,51 +1,80 @@
+export async function FetchEditAppointmentDetailsAPI(
+  appointmentID: string | null,
+) {
+  console.log(appointmentID);
+  if (appointmentID) {
+    const dentistDetails = await fetch(
+      "http://127.0.0.1:8000/api/specified-appointment",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ appointmentID }),
+      },
+    );
+    const data = await dentistDetails.json(); // only once
+    console.log("Appointment data:", data);
 
-export async function FetchEditAppointmentDetailsAPI(appointmentID:string | null){
-  console.log(appointmentID)
-  if(appointmentID){
-   const dentistDetails = await fetch('http://127.0.0.1:8000/api/specified-appointment',{
-     method:"POST",
-     headers:{"Content-Type":"application/json"},
-     body: JSON.stringify({ appointmentID })
-   })
-     const data = await dentistDetails.json() // only once
-     console.log("Appointment data:", data)
- 
-     return data
+    return data;
   }
 }
 
-
+// For Patient Updates
 export async function UpdateAppointment(
-  appointmentID: string| null,
-  scheduleID:string | null,
+  appointmentID: string | null,
+  scheduleID: string | null,
   baseDate: Date,
-  isEmergency:boolean,
-  isFamilyBooking:boolean,
-  message:string,
-){
+  isEmergency: boolean,
+  isFamilyBooking: boolean,
+  message: string,
+) {
   const init_date = new Date(baseDate);
-  const date = init_date.toLocaleDateString("en-CA"); 
+  const date = init_date.toLocaleDateString("en-CA");
 
-  console.log(date)
-  
-  if(appointmentID){
-   const dentistDetails = await fetch('http://127.0.0.1:8000/api/update-appointment',{
-     method:"POST",
-     headers:{"Content-Type":"application/json"},
-     body: JSON.stringify({ 
-       appointmentID, 
-       scheduleID,
-       date,
-       isEmergency,
-       isFamilyBooking,
-       message
-     })
-   })
-     const data = await dentistDetails.json() // only once
-     console.log("Appointment data:", data)
- 
-     return data
+  console.log(date);
+
+  if (appointmentID) {
+    const dentistDetails = await fetch(
+      "http://127.0.0.1:8000/api/update-appointment",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          appointmentID,
+          scheduleID,
+          date,
+          isEmergency,
+          isFamilyBooking,
+          message,
+        }),
+      },
+    );
+    const data = await dentistDetails.json(); // only once
+    console.log("Appointment data:", data);
+
+    return data;
   }
 }
 
-export default {FetchEditAppointmentDetailsAPI, UpdateAppointment}
+//  For dentist updates
+
+export async function UpdateDentistAppointment(
+  appointment_id: string | null,
+  status: string,
+) {
+  console.log(status);
+  const update = await fetch(
+    "http://127.0.0.1:8000/api/edit-appointment-dentist",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ appointment_id, status }),
+    },
+  );
+  return update.json();
+}
+
+export default {
+  FetchEditAppointmentDetailsAPI,
+  UpdateAppointment,
+  UpdateDentistAppointment,
+};
