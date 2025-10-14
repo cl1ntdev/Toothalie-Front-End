92c2c6418ff0647d8ba3d8b8f490b98829a068d6
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-14 23:26:43 +0800

    initial pages to be continued

---
+++ src/Pages/Authenticated/Panes/DentistPane/History.tsx
@@ -0,0 +1,11 @@
+import React, { useState,useEffect } from "react";
+
+
+export default function HistoryPaneDentist() {
+ 
+  return (
+    <>
+      <h1>HISTORY OF APPROVED APPOINTMENTS</h1>
+    </>
+  );
+}
