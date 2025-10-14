ec187ed138126aa03b65238bde99f72672ff03ed
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-14 22:14:12 +0800

    get info for loginned dentist

--- src/API/Authenticated/GetDentist.ts
+++ src/API/Authenticated/GetDentist.ts
@@ -1,9 +1,20 @@
-export default async function getAllDentist(){
+export async function getAllDentist(){
   const res = await fetch('http://127.0.0.1:8000/api/dentists');
-  
+
   if (!res.ok) {
     throw new Error("Failed to fetch dentists");
   }
 
   return res.json();
 };
+
+export async function getDentistData(dentistID: string){
+  const result = await fetch('http://127.0.0.1:8000/api/dentist-info',{
+    method:"POST",
+    headers: { "Content-Type":"application/json"},
+    body:JSON.stringify({dentistID})
+  })
+  
+  return result.json()
+
+}
