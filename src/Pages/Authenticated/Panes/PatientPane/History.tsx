004d1dbab14e07d66db44731899fbca3caf9bd8a
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-29 11:03:33 +0800

    initial for history feeds patient

--- src/Pages/Authenticated/Panes/PatientPane/History.tsx
+++ src/Pages/Authenticated/Panes/PatientPane/History.tsx
@@ -1,8 +1,26 @@
 import React, { useState,useEffect } from "react";
+import { FetchAppointment } from "@/API/Authenticated/appointment/FetchAppointment";
 
 
 export default function HistoryPane() {
- 
+  
+  const [appointmentData, setAppointmentData] = useState()
+  
+  useEffect(()=>{
+    
+    const getAppointment = async() => {
+      const appointments = await FetchAppointment()
+      if(appointments){
+        setAppointmentData(appointments)
+      }
+      console.log(appointments)
+    }
+    
+    getAppointment();
+  },[])
+  
+  
+  
   return (
     <>
       <h1>HISTORY OF APPROVED APPOINTMENTS</h1>
