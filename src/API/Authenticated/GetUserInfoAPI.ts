c7c34c519e575fb43f3e21d8b5091bbaba009516
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-11-10 14:40:22 +0800

    jwt token

--- src/API/Authenticated/GetUserInfoAPI.ts
+++ src/API/Authenticated/GetUserInfoAPI.ts
@@ -1,8 +1,14 @@
 export default async function GetUserInfo(id:string){
   
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+  const token = userInfo.token
+  console.log(token)
   const userData = await fetch("http://127.0.0.1:8000/api/get-user-info",{
     method: "POST",
-    headers: {"Content-Type":"application/json"},
+    headers: {
+      "Content-Type":"application/json",
+      'Authorization': `Bearer ${token}`, 
+    },
     body: JSON.stringify({id})
   })
   
--- src/API/Authenticated/appointment/SubmitAppointment.ts
+++ src/API/Authenticated/appointment/SubmitAppointment.ts
@@ -9,16 +9,23 @@ export default async function SubmitAppointment(
   message:string
 
 ){
-  console.log(patientID)
-  console.log(dentistID)
-  console.log(day)
-  console.log(time)
-  console.log(emergency)
-  console.log(familyBooking)
-  console.log(date)
+  // console.log(patientID)
+  // console.log(dentistID)
+  // console.log(day)
+  // console.log(time)
+  // console.log(emergency)
+  // console.log(familyBooking)
+  // console.log(date)
+  
+const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+const token = userInfo.token
+  
   const submit = await fetch('http://127.0.0.1:8000/api/add-appointment',{
     method:"POST",
-    headers:{"Content-Type":"application/json"},
+    headers:{
+      "Content-Type":"application/json",
+      "Authorization": `Bearer ${token}`, // ⬅️ send token in Authorization header
+    },
     body: JSON.stringify({
       patientID, 
       dentistID,
@@ -30,6 +37,10 @@ export default async function SubmitAppointment(
       message
     })
   })
+  if(!submit.ok){
+    const errorData = await submit.json();
+    throw new Error(errorData.message || "Failed to submit appointment");
+  }
   
   console.log(submit)
   return submit
