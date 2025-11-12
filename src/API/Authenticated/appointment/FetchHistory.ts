deafedecc2954cabd62d640b928d5b853b3d0802
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-11-12 18:21:39 +0800

    send role when fetching history

--- src/API/Authenticated/appointment/FetchHistory.ts
+++ src/API/Authenticated/appointment/FetchHistory.ts
@@ -1,12 +1,16 @@
 
-const fetchHistory = async(patientID: string | null) => {
+const fetchHistory = async(userID: string | null,roleInput:string) => {
+  console.log(userID)
+  const role = roleInput.toString()
   const result = await fetch('http://127.0.0.1:8000/api/get-history',{
     method:"POST",
     headers:{"Content-Type":"application/json"},
-    body: JSON.stringify({patientID})
+    body: JSON.stringify({userID,role})
   })
   
-  return result.json()
+  const data = await result.json() 
+  console.log(data)
+  return data
   
 }
 
