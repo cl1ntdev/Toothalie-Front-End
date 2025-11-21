aafad6757ac6a73cd693e49ed1937ab8479b2123
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-11-22 07:28:45 +0800

    /user protected route

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,28 @@ import LoginPage from './Pages/Auth/LoginPage';
 import  RegisterPage  from './Pages/Auth/RegisterPage';
 import PatientPanel from './Pages/Authenticated/Panes/PatientPane/Patient';
 import DentistPanel from './Pages/Authenticated/Panes/DentistPane/Dentist';
+import ProtectedRoute from './components/ProtectedRoute';
 import UserDashboard from './Pages/Authenticated/UserDashboard';
 function App() {
 
   return (
     <>
       <Routes>
+        {/* <Route path='/user/:id' element={<UserPage />} /> {/* use LINK to navigate and then useParams to get the value */} 
         <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        {/* <Route path='/user/:id' element={<UserPage />} /> {/* use LINK to navigate and then useParams to get the value */} 
-        <Route path='/user' element={<UserDashboard />} />
-        <Route path='/patient/:id' element={<PatientPanel />} />
-        <Route path='/dentist/:id' element={<DentistPanel />} />
+       
+       
+        <Route path='/user' element={
+          <ProtectedRoute>
+            <UserDashboard />          
+          </ProtectedRoute>
+        } />
+       
+       
+        {/*<Route path='/patient/:id' element={<PatientPanel />} />
+        <Route path='/dentist/:id' element={<DentistPanel />} />*/}
       </Routes>
     </>
   )
