c78847ec0b7832b9a50cd98beb2f87d1221314a4
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-11-25 21:58:26 +0800

    api for registration

---
+++ src/API/Authenticated/RegisterUser.ts
@@ -0,0 +1,37 @@
+
+
+export async function registerUser(
+  email: string,
+  role: string[],
+  password: string,
+  username: string,
+  first_name: string,
+  last_name: string,
+  created_at: string,
+){
+  console.log(email)
+  console.log(role)
+  console.log(password)
+  console.log(username)
+  console.log(first_name)
+  console.log(last_name)
+  console.log(created_at)
+  
+  const result = await fetch('http://127.0.0.1:8000/api/register',{
+    method:'POST',
+    headers: { 'Content-Type':'application/json'},
+    body: JSON.stringify({
+      email,
+      role,
+      password,
+      username,
+      first_name,
+      last_name,
+      created_at,      
+    })
+  })
+  
+  const data = await result.json();
+  console.log(data)
+  
+}
