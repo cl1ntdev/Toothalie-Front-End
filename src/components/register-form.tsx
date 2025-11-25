7d3db948f6ac3cff9be545af5992675951feddef
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-11-25 21:58:06 +0800

    added vales to register

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,162 +1,136 @@
-// NOT FINISHED, LATER FOR REGISTER AND AUTH
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react"
-import Patient from "@/Classes/Authenticated/Patient"
-import RegisterAuth from "@/API/RegisterAuth"
-
-export function RegisterForm({
-  className,
-  ...props
-}: React.ComponentProps<"form">) {
-  const [userName,setUsername] = useState<string>("")
-  const [password,setPassword] = useState<string>("")
-  const [confPassword,setConfPassword] = useState<string>("")
-  const [firstName,setFirstName] = useState<string>("")
-  const [lastName,setLastName] = useState<string>("")
-  const [contactNo,setContactNo] = useState<string>("")
-  const [email,setEmail] = useState<string>("")
-  
+import { registerUser } from "@/API/Authenticated/RegisterUser"
+export function RegisterForm({ className, ...props }: React.ComponentProps<"form">) {
   const navigate = useNavigate()
-  
-  const handleRegister = async() =>{
-    console.log('working')
-    if(password.trim() == confPassword.trim()){
-      const user = new Patient(userName,firstName,lastName,password,contactNo,email)
-      const UserLoginData = await RegisterAuth(user)
-      console.log("userlogininfo," + UserLoginData)
-      if(UserLoginData){
-        alert("account exists")
-      }else{
-        navigate(`/user/${UserLoginData}`)        
-      }
-  
-    }
+
+  const [form, setForm] = useState({
+    username: "",
+    first_name: "",
+    last_name: "",
+    contact_no: "",
+    email: "",
+    password: "",
+    confPassword: "",
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.id]: e.target.value })
   }
-  
+
+  const handleRegister = async () => {
+    if (form.password.trim() !== form.confPassword.trim()) {
+      alert("Passwords do not match.")
+      return
+    }
+
+    const created_at = new Date().toISOString()
+    const role = ["ROLE_PATIENT"] 
+
+    const res = await registerUser(
+      form.email,
+      role,
+      form.password,
+      form.username,
+      form.first_name,
+      form.last_name,
+      created_at
+    )
+
+    console.log("Register Response:", res)
+
+    if (!res) {
+      alert("Registration failed")
+      return
+    }
+
+    alert("Account successfully created!")
+    navigate(`/login`)
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)}
-     onSubmit={(e)=>{
-       e.preventDefault()
-       handleRegister()
-     }}
-      {...props}>
+    <form
+      className={cn("flex flex-col gap-6", className)}
+      onSubmit={(e) => {
+        e.preventDefault()
+        handleRegister()
+      }}
+      {...props}
+    >
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Create your account</h1>
         <p className="text-muted-foreground text-sm text-balance">
           Fill out the details below to register your account
         </p>
       </div>
-    
+
       <div className="grid gap-6">
-    
-        {/* Username */}
-       
+        
         <div className="grid gap-3">
           <Label htmlFor="username">Username</Label>
-          <Input
-            id="username"
-            type="text"
-            placeholder="Enter username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+          <Input id="username" type="text" placeholder="Enter username"
+            onChange={handleChange} required />
         </div>
-    
-        <div className="grid-cols-2 flex gap-2 ">
-          <div className="">
-            <Label htmlFor="first_name" className="mb-2">First Name</Label>
-            <Input
-              id="first_name"
-              type="text"
-              placeholder="Enter your first name"
-              onChange={(e) => setFirstName(e.target.value)}
-              required
-            />
+
+        <div className="flex gap-2">
+          <div className="grid gap-3 w-full">
+            <Label htmlFor="first_name">First Name</Label>
+            <Input id="first_name" type="text" placeholder="Enter your first name"
+              onChange={handleChange} required />
           </div>
-      
-          <div className="grid">
-            <Label htmlFor="last_name" className="mb-2">Last Name</Label>
-            <Input
-              id="last_name"
-              type="text"
-              placeholder="Enter your last name"
-              onChange={(e) => setLastName(e.target.value)}
-              required
-            />
+
+          <div className="grid gap-3 w-full">
+            <Label htmlFor="last_name">Last Name</Label>
+            <Input id="last_name" type="text" placeholder="Enter your last name"
+              onChange={handleChange} required />
           </div>
         </div>
-        
-    
-    
-        <div className="grid-cols-2 flex">
-          {/* Contact Number */}
-          <div className="px-2">
-            <Label htmlFor="contact_no" className="mb-2">Contact Number</Label>
+
+        <div className="flex gap-2">
+          <div className="grid gap-3 w-full">
+            <Label htmlFor="contact_no">Contact Number</Label>
             <Input
               id="contact_no"
               type="text"
-              placeholder="e.g. 09XXXXXXXXX"
-              onChange={(e) => setContactNo(e.target.value)}
+              placeholder="09XXXXXXXXX"
+              maxLength={11}
+              pattern="^09[0-9]{9}$"
+              onChange={handleChange}
               required
             />
+
           </div>
-      
-          {/* Email */}
-          <div className="px-2">
-            <Label htmlFor="email" className="mb-2">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+
+          <div className="grid gap-3 w-full">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com"
+              onChange={handleChange} required />
           </div>
-      
         </div>
-        
-        {/* Password */}
+
         <div className="grid gap-3">
           <Label htmlFor="password">Password</Label>
-          <Input
-            id="password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <Input id="password" type="password" onChange={handleChange} required />
         </div>
-    
-        {/* Confirm Password */}
+
         <div className="grid gap-3">
-          <Label htmlFor="conf-password">Confirm Password</Label>
-          <Input
-            id="conf-password"
-            type="password"
-            onChange={(e) => setConfPassword(e.target.value)}
-            required
-          />
+          <Label htmlFor="confPassword">Confirm Password</Label>
+          <Input id="confPassword" type="password" onChange={handleChange} required />
         </div>
-    
-        
+
         <Button type="submit" className="w-full">
           Register
         </Button>
-    
-       
       </div>
-    
+
       <div className="text-center text-sm">
         Already have an account?{" "}
-        <Link to="/login" className="underline underline-offset-4">
-          Login
-        </Link>
+        <Link to="/login" className="underline underline-offset-4">Login</Link>
       </div>
     </form>
-
   )
 }
