69e8d4b651e21f683518ef4d5ccbcae06e5f0e08
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-11-25 22:47:02 +0800

    use symfony to identify user not use params anymore

--- src/API/Authenticated/appointment/FetchAppointment.ts
+++ src/API/Authenticated/appointment/FetchAppointment.ts
@@ -1,21 +1,16 @@
 async function FetchAppointment(){
-  const userID = localStorage.getItem("userID")
   const userInfo = JSON.parse(localStorage.getItem('userInfo'))
-  console.log('userID is: '+ userID)
-  if(userID){
-   const fetchAppointmentDetails = await fetch('http://127.0.0.1:8000/api/get-appointment',{
-     method:"POST",
-     headers:{
-       "Content-Type":"application/json",
-       "Authorization": `Bearer ${userInfo.token}`
-     },
-     body:JSON.stringify({userID})
-   })
-   const data = await fetchAppointmentDetails.json() // only once
-     console.log("Appointment data:", data)
- 
-     return data
-  }
+  const fetchAppointmentDetails = await fetch('http://127.0.0.1:8000/api/get-appointment',{
+    method:"GET",
+    headers:{
+      "Content-Type":"application/json",
+      "Authorization": `Bearer ${userInfo.token}`
+    },
+  })
+  const data = await fetchAppointmentDetails.json()
+    console.log("Appointment data:", data)
+
+    return data
   
 }
 
