f67686e07572b01d838e6d8e7b53c47f69e827ca
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-12-06 18:58:41 +0800

    init

---
+++ src/Pages/Authenticated/Panes/Admin/AppUser.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import GetUser
+export default function AppUser(){
+  useEffect(()=>{
+    const f = async() => {
+      
+    }
+    f()
+  },[])
+  return(
+    <>
+      
+    </>
+  )
+}
