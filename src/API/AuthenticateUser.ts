8205844ae71012ced0e41a74169e55a9e7521c56
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-12-06 15:58:41 +0800

    for checking

--- src/API/AuthenticateUser.ts
+++ src/API/AuthenticateUser.ts
@@ -1,4 +1,5 @@
 export async function authenticateUser(token: string) {
+  console.log(token)
   try {
     const res = await fetch('http://127.0.0.1:8000/api/auth/me', {
       headers: { Authorization: `Bearer ${token}` },
