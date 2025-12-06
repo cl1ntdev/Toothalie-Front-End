a5c5c24cd71c5b62bd52ce2a68306fa46a75dea5
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-12-06 21:52:05 +0800

    crud for users

--- src/API/Authenticated/admin/AppUser.ts
+++ src/API/Authenticated/admin/AppUser.ts
@@ -11,3 +11,41 @@ export async function getUsers(){
   const data = await result.json()
   return data;
 }
+
+
+export async function getUser(userID:string){
+  console.log('test')
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+  console.log(userInfo)
+  const token = (userInfo.token).toString()
+  console.log(token)
+  const result = await fetch('http://127.0.0.1:8000/api/get-user',{
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({userID})
+  })
+  const data = await result.json()
+  return data;
+}
+
+
+export async function updateUser(payload){
+  console.log('test')
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+  console.log(userInfo)
+  const token = (userInfo.token).toString()
+  console.log(token)
+  const result = await fetch('http://127.0.0.1:8000/api/update-user',{
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify(payload)
+  })
+  const data = await result.json()
+  return data;
+}
