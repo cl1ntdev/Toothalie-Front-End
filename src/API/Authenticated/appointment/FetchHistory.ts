b53f1f00a717292f9b49dc7bdb3f69100415e9d7
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-12-10 00:18:17 +0800

    init api

--- src/API/Authenticated/appointment/FetchHistory.ts
+++ src/API/Authenticated/appointment/FetchHistory.ts
@@ -1,21 +1,16 @@
-
-const fetchHistory = async(userID: string | null,roleInput:string) => {
-  console.log(userID)
-  const userInfo = JSON.parse(localStorage.getItem('userInfo'))
-  const role = roleInput.toString()
-  const result = await fetch('http://127.0.0.1:8000/api/get-history',{
-    method:"POST",
-    headers:{
-      "Content-Type":"application/json",
-      'Authorization':`Bearer ${userInfo.token}`
+const fetchHistory = async (userID: string, role: string) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+console.log(role)
+  const result = await fetch("http://127.0.0.1:8000/api/get-history", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${userInfo.token}`,
     },
-    body: JSON.stringify({userID,role})
-  })
-  
-  const data = await result.json() 
-  console.log(data)
-  return data
-  
-}
+    body: JSON.stringify({ userID, role }),
+  });
 
-export { fetchHistory }
+  return await result.json();
+};
+
+export { fetchHistory };
